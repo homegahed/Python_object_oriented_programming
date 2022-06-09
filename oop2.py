051bd149aefa287d752131dc7aad16deb0dff591
Hossam Megahed <hosammegahed91@gmail.com>
2022-06-09 08:38:51 +0400

    OOP2

---
+++ oop2.py
@@ -0,0 +1,17 @@
+
+# Create Basic Class
+class book:
+    def __init__(self, title):
+        self.title = title
+
+
+
+# Create instance of a class
+
+b1 = book('Brave New world')
+b2 = book('War and peace')
+
+
+# print class and property 
+print(b1)
+print(b1.title)
